47cff18b703939fddbbae27f2dd5f50b5bbb10b8
Márcio Oliveira <marcio@MacBook-Pro-de-Marcio.local>
2025-07-28 22:54:12 -0300

    Add Suspense in layout for useSearchParams of quiz page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { Suspense } from 'react'
 import type { Metadata } from 'next';
 
 const inter = Inter({
@@ -19,7 +20,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${inter.variable} bg-[#f3f2f1] text-gray-900`}>
-        {children}
+        <Suspense>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
